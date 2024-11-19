b5e35f17770d1e6f3c0f6f320eb18e9cf0098dec
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2024-11-19 16:48:55 +0100

    04 password provider

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,5 +1,6 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { TriangleAlert } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -11,30 +12,44 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+
 import { SignInFlow } from "../types";
 import { useState } from "react";
 
-import {useAuthActions} from "@convex-dev/auth/react"
-
+import { useAuthActions } from "@convex-dev/auth/react";
 
 interface SignInCardProps {
   setState: (state: SignInFlow) => void;
 }
 
 export const SignInCard = ({ setState }: SignInCardProps) => {
-  const {signIn} = useAuthActions();
+  const { signIn } = useAuthActions();
+
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [pending,setPending] = useState(false)
+  const [error, setError] = useState("");
+  const [pending, setPending] = useState(false);
 
-  const onProviderSignIn = (value:"github"|"google") =>{
-    setPending(true)
-    signIn(value)
-      .finally(()=>{
-        setPending(false)
+  const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setPending(true);
+
+    signIn("password", { email, password, flow: "signIn" })
+      .catch(() => {
+        setError("Invalid email or password");
       })
-  }
+      .finally(() => {
+        setPending(false);
+      });
+  };
+
+  const onProviderSignIn = (value: "github" | "google") => {
+    setPending(true);
+    signIn(value).finally(() => {
+      setPending(false);
+    });
+  };
 
   return (
     <Card className="size-full p-8 ">
@@ -44,8 +59,14 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+          <TriangleAlert className="size-4" />
+          <p>{error}</p>
+        </div>
+      )}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5">
+        <form onSubmit={onPasswordSignIn} className="space-y-2.5">
           <Input
             disabled={pending}
             value={email}
@@ -63,10 +84,10 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
             required
           />
 
-          <Button 
-            disabled={pending} 
-            type="submit" 
-            className="w-full" 
+          <Button
+            disabled={pending}
+            type="submit"
+            className="w-full"
             size={"lg"}
           >
             Continue
